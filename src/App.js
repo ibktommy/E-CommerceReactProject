73b1eff674b94ec10f6fbfce1ecf8b674f5f2f4d
ibktommy <atomquartz@gmail.com>
2022-05-05 17:56:50 +0100

    Creating Header Component containing Logo and Links to Other Pages

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import "./App.css";
 
 import Homepage from "./pages/homepage/homepageComponents.jsx";
 import ShopPage from "./pages/shop/shopComponent";
+import Header from "./components/header/headerComponent";
 
 function App() {
 	return (
 		<>
+			<Header />
 			<Routes>
 				<Route exact path="/" element={<Homepage />}></Route>
 				<Route exact path="/shop" element={<ShopPage />}></Route>
---
+++ src/components/header/headerComponent.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { ReactComponent as Logo } from "../../assets/4.1 crown.svg";
+
+import "./headerStyle.scss";
+
+const Header = () => (
+	<div className="header">
+		<Link to={"/"} className="logo-container">
+			<Logo className="logo" />
+		</Link>
+
+		<div className="options">
+			<Link to={"/shop"} className="option">
+				SHOP
+			</Link>
+			<Link to={"/contact"} className="option">
+				CONTACT
+			</Link>
+		</div>
+	</div>
+);
+
+export default Header;
